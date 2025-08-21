6e610fb40663e37b206928229f3043f364bc3543
Kiril Kartunov <kiril.kartunov@gmail.com>
2025-08-21 08:59:42 +0300

    switch to UUID

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { McpModule } from '@tc/mcp-nest';
 import { HealthCheckController } from './api/health-check/healthCheck.controller';
 import { TokenValidatorMiddleware } from './core/auth/middleware/tokenValidator.middleware';
-import { nanoid } from 'nanoid';
 import { ToolsModule } from './mcp/tools/tools.module';
 import { GlobalProvidersModule } from './shared/global/globalProviders.module';
 import { ResourcesModule } from './mcp/resources/resources.module';
+import { randomUUID } from 'crypto';
 
 @Module({
   imports: [
@@ -14,7 +14,7 @@ import { ResourcesModule } from './mcp/resources/resources.module';
       version: '1.0.0',
       streamableHttp: {
         enableJsonResponse: false,
-        sessionIdGenerator: () => nanoid(),
+        sessionIdGenerator: () => randomUUID(),
         statelessMode: false,
       },
     }),
