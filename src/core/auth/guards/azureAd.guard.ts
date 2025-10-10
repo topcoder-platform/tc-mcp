2e35abd19332d72ca9a407dda1a1d578141d971c
Ravi80335 <Ravisankar.Bommu@gmail.com>
2025-10-10 22:17:07 +0530

    module load issue fix

--- src/core/auth/guards/azureAd.guard.ts
+++ src/core/auth/guards/azureAd.guard.ts
@@ -5,18 +5,20 @@ import {
     UnauthorizedException,
     ForbiddenException,
   } from '@nestjs/common';
-  import jwt, { JwtHeader, SigningKeyCallback } from 'jsonwebtoken';
+  import * as jwt from 'jsonwebtoken';
+  import { JwtHeader, SigningKeyCallback } from 'jsonwebtoken';
   import { JwksClient } from 'jwks-rsa';
   import { ENV_CONFIG } from 'src/config';
-  
+
   // Use the new, validated config variable
   const mockAzureAdValidation = ENV_CONFIG.MOCK_AZURE_AD_VALIDATION;
-  if (mockAzureAdValidation) console.warn('Mock Azure AD validation enabled 🚀');
-  
+  if (mockAzureAdValidation)
+    console.warn('Mock Azure AD validation enabled 🚀');
+
   const multiTenantClient = new JwksClient({
     jwksUri: `https://login.microsoftonline.com/common/discovery/v2.0/keys`,
   });
-  
+
   const getSigningKey = (header: JwtHeader, callback: SigningKeyCallback) => {
     if (!header.kid) {
       return callback(new Error('JWT header is missing "kid" property.'));
@@ -29,24 +31,24 @@ import {
       callback(null, signingKey);
     });
   };
-  
+
   @Injectable()
   export class AzureAdGuard implements CanActivate {
     async canActivate(context: ExecutionContext): Promise<boolean> {
       const request = context.switchToHttp().getRequest();
       const authHeader = request.headers.authorization;
-  
+
       if (!authHeader || !authHeader.startsWith('Bearer ')) {
         throw new UnauthorizedException('No token provided.');
       }
-  
+
       const token = authHeader.substring(7);
-  
+
       if (mockAzureAdValidation) {
         request.user = { oid: token };
         return true;
       }
-  
+
       try {
         const payload = await this.verifyToken(token);
         request.user = payload;
@@ -55,14 +57,14 @@ import {
         throw error;
       }
     }
-  
+
     private verifyToken(token: string): Promise<jwt.JwtPayload> {
       return new Promise((resolve, reject) => {
         jwt.verify(
           token,
           getSigningKey,
           {
-            audience: ENV_CONFIG.AZURE_AD_AUDIENCE, // Corrected reference
+            audience: ENV_CONFIG.AZURE_AD_AUDIENCE,
             algorithms: ['RS256'],
           },
           (err, decoded) => {
@@ -71,9 +73,9 @@ import {
                 new UnauthorizedException('Invalid token.', err.message),
               );
             }
-  
+
             const payload = decoded as jwt.JwtPayload;
-  
+
             if (
               ENV_CONFIG.IS_SAME_AZURE_AD_TENANT && // Corrected reference
               payload.tid !== ENV_CONFIG.AZURE_AD_TENANT_ID // Corrected reference
@@ -82,7 +84,7 @@ import {
                 new ForbiddenException('User is not from the correct tenant.'),
               );
             }
-  
+
             resolve(payload);
           },
         );
