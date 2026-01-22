50bf4a65be588e9de8c755546ca8dc6e3354dd77
Ravi80335 <Ravisankar.Bommu@gmail.com>
2026-01-22 20:39:49 +0530

    preprod agent url

--- teamsTab/src/config/prod.ts
+++ teamsTab/src/config/prod.ts
@@ -1,5 +1,5 @@
 export const config = {
-  apiBaseUrl: "https://example.com/v6/mcp/agent",
+  apiBaseUrl: 'https://d2mom6cer8xmr1.cloudfront.net/v6/mcp/agent',
   isTeamsTab: false,
-  mockAzureADtoken: true
+  mockAzureADtoken: true,
 };
