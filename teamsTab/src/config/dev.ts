fc8ed6ae2729958e99fa3db9e04cca9b77b9737a
Ravi80335 <Ravisankar.Bommu@gmail.com>
2026-01-22 22:13:33 +0530

    env configured, cleanup

--- teamsTab/src/config/dev.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  apiBaseUrl: "https://example-dev.com/v6/mcp/agent",
-  isTeamsTab: false,
-  mockAzureADtoken: false
-};
--- teamsTab/src/config/prod.ts
+++
@@ -1,5 +0,0 @@
-export const config = {
-  apiBaseUrl: 'https://d2mom6cer8xmr1.cloudfront.net/v6/mcp/agent',
-  isTeamsTab: false,
-  mockAzureADtoken: true,
-};
